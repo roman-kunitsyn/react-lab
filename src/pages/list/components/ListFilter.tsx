2af80840c41a9e9173830a8fb2a9a0de4c415c70
Roman Kunitsyn <roman.kunitsyn@gmail.com>
2025-02-21 00:09:17 +0200

    workflow

--- src/pages/list/components/ListFilter.tsx
+++ src/pages/list/components/ListFilter.tsx
@@ -9,19 +9,19 @@ export const ListFilter = ({
       <code>ListFilter</code>
       <form>
         <div>
-          <span>ids:</span> <input type="" name="ids" />
-          <span>name:</span> <input type="" name="name" />
-          <span>description:</span>{' '}
-          <input type="" name="description" />
           <div>
             <span>createdAt:</span> <span>Start </span>{' '}
             <input type="date" name="createdAtStart" />
             <span>End</span> <input type="date" name="createdAtEnd" />
           </div>
+          <span>ids:</span> <input type="" name="ids" />
+          <span>name:</span> <input type="" name="name" />
+          <span>description:</span>{' '}
+          <input type="" name="description" />
         </div>
         <div>
-          <input type="submit" value="Submit" />
-          <input type="reset" value="Reset" />
+          <input type="reset" value="Clear" />
+          <input type="submit" value="Filter" />
         </div>
       </form>
     </div>
